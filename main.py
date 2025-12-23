22d5d71d1f06ff3bddd38cef2544cc902feddf2d
Keith <keithkipngeno24@gmail.com>
2025-12-23 22:43:36 +0300

    Added git initialisation for the project. Lets users push to git immediately

--- main.py
+++ main.py
@@ -83,6 +83,57 @@ def install_laravel(project_path):
         return False
     
 
+def initialise_git(project_path):
+    tool = 'git'
+    tool_path = shutil.which(tool)
+    
+    if not tool_path:
+        print(f"{tool} not found in PATH")
+        return False
+    
+    print(f"{tool} found at {tool_path} ---- ok")
+
+    run_kwargs = {
+        'cwd': project_path,
+        'check': True,
+        'stdout': subprocess.PIPE,
+        'stderr': subprocess.PIPE,
+        'text': True
+    }
+
+
+    # change wd, run git commands
+    try:
+        subprocess.run([
+            tool,
+            "init"
+        ],**run_kwargs)
+        print(f"Git repository initialised in {project_path} using CLI.")
+
+        repo_link = input("Enter link to github repo (leave blank to skip): ").strip()
+
+        if repo_link:
+            subprocess.run([
+                tool, 'add', '.'
+            ],**run_kwargs)
+            git_commit = input("Enter first commit message (leave blank for 'First commit'): ")
+            commit_msg = git_commit if git_commit else "First commit"
+
+            subprocess.run([tool,"commit","-m",commit_msg],**run_kwargs)
+            subprocess.run([tool,"branch","-M","main"],**run_kwargs)
+            subprocess.run([tool, "remote", "add", "origin", repo_link], **run_kwargs)
+            subprocess.run([tool, "push", "-u", "origin", "main"], **run_kwargs)
+            print(f"Successfully pushed to remote repo {repo_link}")
+        else:
+            print("Skipping remote repository setup.")
+        return True
+    except subprocess.CalledProcessError as e:
+        print(f"Error with Git operation: {e.stderr}")
+        return False
+
+# if initialise_git(project_path='C:\\Users\\User\\OneDrive\\Desktop\\LARA_PROJECTS\\test2'):
+#     create_project_folder()
+
 def main():
     print("=== Laravel Setup Automation ===\n")
     
@@ -108,6 +159,10 @@ def main():
         print(f"\nSetup complete! Your Laravel project is at: {project_path}")
     else:
         print("\n✗ Laravel installation failed.")
+    
+    print("Step 4: Initialising git repository")
+    if initialise_git(project_path):
+        print(f'Set up complete.')
 
 if __name__ == "__main__":
     main()
